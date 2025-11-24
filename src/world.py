3b576aaf86644c27b67356fd34f08e9b14f18f52
Dennis Sauve <dengsauve@yahoo.com>
2025-11-23 22:54:06 -0800

    initial world build complete

--- src/world.py
+++ src/world.py
@@ -6,14 +6,38 @@ def build_world():
             id="home",
             name="Your House, in the middle of your street",
             description="Homebase, where everything is. In the garage you have a bow and arrow.",
-            adjacent_levels={"south": "lincoln_park"}
+            adjacent_levels={"north": "lincoln_park"}
         ),
         "lincoln_park": Level(
             id="lincoln_park",
             name="Lincoln Park",
             description="A beautiful walking park with a nice pond. Plenty of turkeys about.",
-            adjacent_levels={"north": "home"}
-        )
+            adjacent_levels={"south": "home", "west": "medical_park"}
+        ),
+        "medical_park": Level(
+            id="medical_park",
+            name="Medical Park",
+            description="A modest set of parks set in between Spokane's hospitals.",
+            adjacent_levels={"east": "lincoln_park", "west": "cda_park", "south": "manito_park"}
+        ),
+        "cda_park": Level(
+            id="cda_park",
+            name="Couer d'Alene Park",
+            description="A lovely park in Spokane's Browne's Addition neighborhood.",
+            adjacent_levels={"east": "medical_park"}
+        ),
+        "manito_park": Level(
+            id="manito_park",
+            name="Manito Park",
+            description="Spokane's premier park, featuring ponds, rose gardens, courtyards, and a greenhouse conservatory.",
+            adjacent_levels={"north": "medical_park", "west": "cliff_cannon_park"}
+        ),
+        "cliff_cannon_park": Level(
+            id="cliff_cannon_park",
+            name="Cliff Cannon Park",
+            description="A small park surrounded by Spokane's elite.",
+            adjacent_levels={"east": "manito_park"}
+        ),
     }
 
     # second pass: resolve neighbor references
